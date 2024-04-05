1cf896c4e96e95cd699a4b330b5c5dddc3d3ed40
SerenaCristian <devcristian02@gmail.com>
2024-04-04 22:06:20 -0300

    Exercicio de Revisão

---
+++ Pessoa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProjetoPessoa
+{
+    public class Pessoa
+    {
+        public string Nome;
+        public double Altura;
+        public int Idade;
+
+        public Pessoa(string nome, double altura, int idade) 
+        {
+            Nome = nome;
+            Altura = altura;
+            Idade = idade;
+        
+        }
+
+        public void Apresentar()
+        {
+            Console.WriteLine($"\nO Nome Digitado Foi: {Nome} ");
+            Console.WriteLine($"\nA altura digitada Foi: {Altura} ");
+            Console.WriteLine($"\nA Idade digitada Foi: {Idade} ");
+            
+        }
+            
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ProjetoPessoa;
+
+string dot = new string('.', 50);
+Console.WriteLine("Exercicio de Revisão");
+Console.WriteLine(dot);
+
+
+
+Console.Write("Digite o seu Nome: ");
+string nome = Console.ReadLine();
+
+Console.Write("Digite Sua Altura: ");
+double altura = double.Parse(Console.ReadLine());
+
+Console.Write("Digite sua Idade ");
+int idade = int.Parse(Console.ReadLine());
+
+Pessoa p = new Pessoa(nome, altura, idade);
+p.Apresentar();
